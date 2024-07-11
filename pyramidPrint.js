ca29412f7716cc69a73120e841c8f967cc788a68
aladadev <aladadev29@gmail.com>
2024-07-12 02:04:20 +0600

    inverted pyramid mode add

--- pyramidPrint.js
+++ pyramidPrint.js
@@ -1,20 +1,27 @@
 const character = "#";
 const count = 8;
 const rows = [];
+let inverted = true;
 
 function padRow(rowNumber, rowCount) {
-    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
-  }
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
 
 
-  for (let i = 0; i < count; i = i + 1) {
-    rows.push(padRow(i + 1, count));
+
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
   }
+}
+
   
-  let result = ""
-  
-  for (const row of rows) {
-    result = result + "\n" + row;
-  }
-  
-  console.log(result);
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
